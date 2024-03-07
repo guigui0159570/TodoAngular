cc99ef24685efb8c8a713f5a6748742ca07d88c2
Guillaume CODER <guillaume-coder@live.fr>
2024-03-07 10:03:43 +0100

    Create Spring Project

---
+++ todoangular-spring/src/main/java/fr/guigui0159570/todoangular/TodoAngularApplication.java
@@ -0,0 +1,13 @@
+package fr.guigui0159570.todoangular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoAngularApplication.class, args);
+	}
+
+}
